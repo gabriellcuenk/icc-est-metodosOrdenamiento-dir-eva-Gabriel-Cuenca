159a9d9c3e4f3d868811157b7b6084ec7453339d
Gabriel Cuenca <gcuenca119@gmail.com>
2026-05-12 23:45:59 +0000

    Asd

--- src/controllers/CarController.java
+++ src/controllers/CarController.java
@@ -3,16 +3,21 @@ package controllers;
 import models.Car;
 
 public class CarController {
+  boolean intercambio;
 
   public void sortByName(Car[] cars) {
     for (int i = 0; i < cars.length - 1; i++) {
       for (int j = 0; j < cars.length - 1 - i; j++) {
         if (cars[j].getBrand().compareToIgnoreCase(cars[j + 1].getBrand()) > 0) {
           // compara ambos String y si devuelve un resultado >0 significa que cambia
+          intercambio = true;
           Car temp = cars[j];
           cars[j] = cars[j + 1];
           cars[j + 1] = temp;
         }
+      }
+      if (!intercambio) {
+        break;
       }
     }
   }
