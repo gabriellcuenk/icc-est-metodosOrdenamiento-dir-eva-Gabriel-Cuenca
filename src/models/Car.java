220e4077a3495159b2d735d9c90b2e85b8262101
Gabriel Cuenca <gcuenca119@gmail.com>
2026-05-12 23:01:51 +0000

    Ya acabe

--- src/models/Car.java
+++ src/models/Car.java
@@ -1,5 +1,18 @@
 package models;
 
 public class Car {
-  
+
+  private int year;
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public Car(int year) {
+    this.year = year;
+  }
 }
