dee89444179d5ed347218b356fda7b2b75a9d594
Gabriel Cuenca <tuemgcuenca119@gmail.com>
2026-05-18 13:13:55 -0500

    2

--- src/controllers/CarController.java
+++ src/controllers/CarController.java
@@ -7,6 +7,7 @@ public class CarController {
 
   public void sortByName(Car[] cars) {
     for (int i = 0; i < cars.length - 1; i++) {
+      intercambio = false;
       for (int j = 0; j < cars.length - 1 - i; j++) {
         if (cars[j].getBrand().compareToIgnoreCase(cars[j + 1].getBrand()) > 0) {
           // compara ambos String y si devuelve un resultado >0 significa que cambia
